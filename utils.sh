f7e7a72f724411c923b1166ef7b912d228398072
Sharath <sharathsukash@gmail.com>
2026-02-23 20:57:25 +0530

    Update utils.sh
    Signed-off-by: Sharath <sharathsukash@gmail.com>

--- utils.sh
+++ utils.sh
@@ -470,7 +470,7 @@ get_uptodown_pkg_name() { $HTMLQ --text "tr.full:nth-child(1) > td:nth-child(3)"
 dl_archive() {
 	local url=$1 version=$2 output=$3 arch=$4
 	local path version=${version// /}
-	path=$(grep "${version_f#v}-${arch// /}*apk" <<<"$__ARCHIVE_RESP__") || return 1
+	path=$(grep "${version_f#v}-${arch// /}" <<<"$__ARCHIVE_RESP__") || return 1
 	req "${url}/${path}" "$output"
 }
 get_archive_resp() {
